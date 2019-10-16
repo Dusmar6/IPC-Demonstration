937a38c9172e3087c76c1965300e56b790fba9e8
Dusmar6 <dusmar6@gmail.com>
2019-10-16 10:21:33 -0700

    update

--- DataHub.cpp
+++ DataHub.cpp
@@ -3,3 +3,16 @@
 //  ProbeA.cpp, ProbeB.cpp, ProbeC.cpp, DataHub.cpp
 //  This code defines the DataHub component of this
 //  assignment
+
+
+int main(){
+
+    Struct buf{
+    Long mtype; // required
+    Char greeting[50]; //msg content
+    };
+
+
+
+
+}
